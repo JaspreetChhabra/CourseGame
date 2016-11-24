1a4a60a269455bfd5700ec44eda7e414e4f6d45b
mansishah13ms <mansishah.13ms@gmail.com>
2016-11-24 15:15:36 +0530

    new UI

---
+++ master/WebApplicationUI/assets/js/custom/app.js
@@ -0,0 +1,71 @@
+var app=angular.module("myApp",['ui.router','ngResource','ngFileUpload','ngStorage']);
+app.config(function($stateProvider, $urlRouterProvider,$resourceProvider) {
+    
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'login/pages-login.html',
+            controller:'loginctrl'
+        })
+        
+        .state('register',{
+        url: '/register',
+        templateUrl: 'pages-register.html',
+        controller: 'registerctrl'
+    })
+
+        .state('course', {
+            url: '/course',
+            templateUrl: 'course/course.html',
+            controller: 'coursectrl'
+        })
+
+        .state('addGame',{
+        url: '/addGame',
+        templateUrl: 'addGame/addGame.html',
+        controller: 'addGamectrl'
+    })
+        .state('home', {
+            url: '/home',
+            templateUrl: 'login/homepage/pages/index.html',
+            controller:'homectrl'
+        })
+        .state('dashboard', {
+            url: '/Dashboard',
+            templateUrl: 'Dashboard/index.html',
+            controller:'dashboardctrl'
+        })
+
+        .state('upload', {
+            url: '/upload',
+            templateUrl: 'file/fileupload.html',
+            controller:'fileUploadctrl'
+        })
+        .state('unit', {
+            url: '/unit',
+            templateUrl: 'unit/unit.html',
+            controller:'unitctrl'
+        })
+        .state('material', {
+            url: '/material',
+            templateUrl: 'material/material.html',
+            controller:'materialctrl'
+        })
+        .state('announcements', {
+            url: '/announcements',
+            templateUrl: 'announcements/announcements.html',
+            controller:'announcementctrl'
+        })
+
+        
+        
+        $urlRouterProvider.otherwise('/login');
+
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+        
+});
+
+app.config(['$resourceProvider', function($resourceProvider) {
+  // Don't strip trailing slashes from calculated URLs
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
